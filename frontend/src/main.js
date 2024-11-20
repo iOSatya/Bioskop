0d2c23b73bef94947c897bf5fc51da8c0e6c6e1c
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-11-20 17:19:52 +0700

    Initial commit

---
+++ frontend/src/main.js
@@ -0,0 +1,16 @@
+import '@/assets/base.css';
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import { register } from 'swiper/element/bundle';
+
+register();
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
