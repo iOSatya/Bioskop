77417cd54c26eb281f6412bb6c4a0eb6b9ad0ee3
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-11-23 14:00:31 +0700

    Menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,14 @@
 <template>
 
-  <RouterView />
+  <div style="background-color: var(--darker); color: var(--lighter);">
+    <NavigationBar class="fixed z-50" style="backdrop-filter: blur(2px);" />
+    <RouterView />
+  </div>
 
 </template>
+
+<script setup>
+
+  import NavigationBar from '@/components/NavigationBar.vue';
+
+</script>
---
+++ frontend/src/components/NavigationBar.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div class="flex flex-row justify-center items-center w-screen">
+    <nav class="flex flex-row" style="width: 80vw;">
+      <div class="me-auto">
+        <RouterLink :to="{name: 'home'}" class="nav-button">Home</RouterLink>
+      </div>
+      <div>
+        <RouterLink :to="{name: 'register'}" class="nav-button">Register</RouterLink>
+      </div>
+    </nav>
+  </div>
+
+</template>
+
+<style scoped>
+
+  a.router-link-active {
+    border-bottom-color: var(--lighter);
+  }
+
+  .nav-button {
+    font-weight: 600;
+    display: inline-block;
+    padding: 1rem 1.5rem;
+    border-bottom: 2px solid transparent;
+    box-sizing: border-box;
+  }
+
+  .nav-button:hover {
+    color: var(--secondary);
+  }
+
+</style>
