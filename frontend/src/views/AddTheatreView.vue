67ac8c162b82d9d9702dd8e2fa2ca06496f616d5
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-12-04 07:16:02 +0700

    Update

--- frontend/src/views/AddTheatreView.vue
+++ frontend/src/views/AddTheatreView.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="flex flex-col justify-center items-center">
-    <div class="flex flex-row w-full min-h-96 p-12 items-center">
+    <div class="flex flex-row w-full min-h-full p-12 items-center">
       <AddNavigator class="me-12 h-80 self-start" />
       <AddTheatreForm class="w-full" />
     </div>
