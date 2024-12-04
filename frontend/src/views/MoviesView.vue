ddd696b83fd383d60c029c225e0a1522652edb8d
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-12-04 12:32:53 +0700

    Update MoviesView.vue

--- frontend/src/views/MoviesView.vue
+++ frontend/src/views/MoviesView.vue
@@ -1,22 +1,23 @@
 <template>
 
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col justify-center items-center">
 
     <div class="w-5/6">
-      <h1>Currently Airing</h1>
+      <h1 class="text-2xl font-semibold">Currently Airing</h1>
       <hr class="mt-4 mb-4" style="border-color: var(--secondary);">
       <swiper-container slides-per-view="3" space-between="10" free-mode="true" class="w-full">
         <swiper-slide v-for="movie in movieList" :key="movie.id" class="flex flex-col justify-center">
-          <div class="flex flex-col justify-center items-center h-80 mb-2" style="border: 1px solid var(--lighter);">
+          <div class="flex flex-col justify-center items-center h-96 mb-2" style="border: 1px solid var(--lighter);">
             <p>{{ movie.title }}</p>
           </div>
-          <div class="flex flex-row items-center h-12 p-4" style="border: 1px solid var(--yellow);">
-            <p>{{ movie.id }}</p>
+          <div class="flex flex-row justify-start items-center p-4" style="border: 1px solid var(--yellow);">
+            <p>{{ movie.start }}.00 - {{ movie.end }}.00</p>
+            <RouterLink to="/movies/order" class="ms-auto">Order</RouterLink>
           </div>
         </swiper-slide>
       </swiper-container>
     </div>
-    
+
   </div>
   
 </template>
