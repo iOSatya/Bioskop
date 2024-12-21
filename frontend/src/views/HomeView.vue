d312683c56060b765e0bd859393f301a21af9f4a
dwitamara <septyaatid02@gmail.com>
2024-12-22 02:06:21 +0700

    home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,24 @@
 <template>
-
-  <div class="flex flex-col justify-center items-center">
-    Home
+  <div>
+    <HeroSection />
+    <TopFiveMovies />
+    <NewMovies />
+    <Footer />
   </div>
-  
 </template>
+
+<script>
+import HeroSection from "@/components/HeroSection.vue";
+import TopFiveMovies from "@/components/TopFiveMovies.vue";
+import NewMovies from "@/components/NewMovies.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    HeroSection,
+    TopFiveMovies,
+    NewMovies,
+    Footer,
+  },
+};
+</script>
